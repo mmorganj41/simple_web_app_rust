3ab44d3f06db030ad7f9e322042e553ccdd0e811
mmorganj41 <mmorganj41@gmail.com>
2023-02-23 10:24:47 -0500

    modify repo to make route

--- src/api/user_api.rs
+++ src/api/user_api.rs
@@ -1,7 +1,7 @@
 use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
 use actix_web::{
-    post,
-    web::{Data, Json},
+    get, post,
+    web::{Data, Json, Path},
     HttpResponse,
 };
 
@@ -14,6 +14,19 @@ pub async fn create_user(db: Data<MongoRepo>, new_user: Json<User>) -> HttpRespo
         title: new_user.title.to_owned(),
     };
     let user_detail = db.create_user(data).await;
+    match user_detail {
+        Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[get("/user/{id}")]
+pub async fn get_user(db: Data<MongoRepo>, path: Path<String>) -> HttpResponse {
+    let id = path.into_inner();
+    if id.is_empty() {
+        return HttpResponse::BadRequest().body("Invalid Id");
+    }
+    let user_detail = db.get_user(&id).await;
     match user_detail {
         Ok(user) => HttpResponse::Ok().json(user),
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
