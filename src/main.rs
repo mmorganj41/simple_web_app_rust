276c22bfac6c372e03b54692120707536e21f9b0
mmorganj41 <mmorganj41@gmail.com>
2023-02-23 09:22:36 -0500

    update entry point to connect to database, load data, and route create_user

--- src/main.rs
+++ src/main.rs
@@ -3,28 +3,16 @@ mod api;
 mod models;
 mod repository;
 
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web::Data, App, HttpServer};
+use api::user_api::create_user;
+use repository::mongodb_repo::MongoRepo;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .app_data(web::Data::new(AppState {
-                app_name: String::from("Actix Web"),
-            }))
-            .service(index)
-    })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
-}
-
-struct AppState {
-    app_name: String,
-}
-
-#[get("/")]
-async fn index(data: web::Data<AppState>) -> String {
-    let app_name = &data.app_name;
-    format!("Hello {app_name}")
+    let db = MongoRepo::init().await;
+    let db_data = Data::new(db);
+    HttpServer::new(|| App::new().app_data(db_data.clone()).service(create_user))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
