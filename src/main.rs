c06ea16b25ff087b46751bf4b177249fd1d2b096
mmorganj41 <mmorganj41@gmail.com>
2023-02-23 10:25:32 -0500

    modify entry point

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,20 @@ mod models;
 mod repository;
 
 use actix_web::{web::Data, App, HttpServer};
-use api::user_api::create_user;
+use api::user_api::{create_user, get_user};
 use repository::mongodb_repo::MongoRepo;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let db = MongoRepo::init().await;
     let db_data = Data::new(db);
-    HttpServer::new(move || App::new().app_data(db_data.clone()).service(create_user))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .app_data(db_data.clone())
+            .service(create_user)
+            .service(get_user)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
