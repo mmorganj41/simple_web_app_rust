04331fcada8baffdb3274185db98423cb22c09c6
mmorganj41 <mmorganj41@gmail.com>
2023-02-23 10:53:23 -0500

    attach route to entry point

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod models;
 mod repository;
 
 use actix_web::{web::Data, App, HttpServer};
-use api::user_api::{create_user, get_user};
+use api::user_api::{create_user, get_user, update_user};
 use repository::mongodb_repo::MongoRepo;
 
 #[actix_web::main]
@@ -16,6 +16,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(db_data.clone())
             .service(create_user)
             .service(get_user)
+            .service(update_user)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
