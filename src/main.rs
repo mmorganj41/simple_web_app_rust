432ec46a88dc5c49381cb7ecf68e728c726907cb
mmorganj41 <mmorganj41@gmail.com>
2023-02-22 21:24:25 -0500

    app state test

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,22 @@ use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
+            .app_data(web::Data::new(AppState {
+                app_name: String::from("Actix Web"),
+            }))
+            .service(index)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
 }
 
+struct AppState {
+    app_name: String,
+}
+
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello World!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
+async fn index(data: web::Data<AppState>) -> String {
+    let app_name = &data.app_name;
+    format!("Hello {app_name}")
 }
