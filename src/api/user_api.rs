6d6e76262c0e5b0cf59501d7828b9882500bbb79
mmorganj41 <mmorganj41@gmail.com>
2023-02-23 10:52:12 -0500

    make update api route

--- src/api/user_api.rs
+++ src/api/user_api.rs
@@ -1,9 +1,10 @@
 use crate::{models::user_model::User, repository::mongodb_repo::MongoRepo};
 use actix_web::{
-    get, post,
+    get, post, put,
     web::{Data, Json, Path},
     HttpResponse,
 };
+use mongodb::bson::oid::ObjectId;
 
 #[post("/user")]
 pub async fn create_user(db: Data<MongoRepo>, new_user: Json<User>) -> HttpResponse {
@@ -29,6 +30,39 @@ pub async fn get_user(db: Data<MongoRepo>, path: Path<String>) -> HttpResponse {
     let user_detail = db.get_user(&id).await;
     match user_detail {
         Ok(user) => HttpResponse::Ok().json(user),
+        Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+    }
+}
+
+#[put("/user/{id}")]
+pub async fn update_user(
+    db: Data<MongoRepo>,
+    path: Path<String>,
+    new_user: Json<User>,
+) -> HttpResponse {
+    let id = path.into_inner();
+    if id.is_empty() {
+        return HttpResponse::BadRequest().body("Invalid ID");
+    }
+    let data = User {
+        id: Some(ObjectId::parse_str(&id).unwrap()),
+        name: new_user.name.to_owned(),
+        location: new_user.location.to_owned(),
+        title: new_user.title.to_owned(),
+    };
+    let update_result = db.update_user(&id, new_user).await;
+    match update_result {
+        Ok(update) => {
+            if update.matched_count == 1 {
+                let updated_user_info = db.get_user(&id).await;
+                return match updated_user_info {
+                    Ok(user) => HttpResponse::Ok().json(user),
+                    Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
+                };
+            } else {
+                return HttpResponse::NotFound().body("No user found with the specified ID");
+            }
+        }
         Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
     }
 }
